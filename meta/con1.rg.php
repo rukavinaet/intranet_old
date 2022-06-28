43b24011a3ee1702c7b58cd05faeff21f1fbfc62
rukavinaet <git@rukavinaet.com>
2022-06-28 16:04:07 +0200

    start-again:)

--- meta/con1.rg.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$dbServerName = "localhost:3306";
-$dbUser = "masteruser";
-$dbPass = "Sunce888##";
-$dbName = "production1";
-$conn = mysqli_connect($dbServerName, $dbUser, $dbPass, $dbName);
-date_default_timezone_set("Europe/Zagreb");
